47f28f04b917c7f18724247311bdf10d5a3bad29
Murtaza Raza <murtazaraza336@gmail.com>
2024-04-07 19:08:46 +0100

    adding packages

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import find_packages,setup
-from typing import List
 
 HYPEN_E_DOT = '-e .'
-def get_requirements(file_path:str)->List[str]:
+def get_requirements(file_path):
     '''
     this function will return the list of requirements
     '''
