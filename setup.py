f3fcda017d692db76da993fcbd3b7cabdaa7b0f7
Murtaza Raza <murtazaraza336@gmail.com>
2024-04-07 18:20:54 +0100

    updating setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+name='daproject',
+version='0.0.1',
+author='Murtaza',
+author_email='murtazaraza336@gmail.com',
+packages=find_packages(),
+install_requires=['pandas', 'numpy']
+
+)
