d31965fa9739f4d4e069918e6f11e88c829feb67
Murtaza Raza <murtazaraza336@gmail.com>
2024-04-07 18:49:43 +0100

    connecting setup.py with requirements file

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
 setup(
 name='daproject',
@@ -6,6 +22,5 @@ version='0.0.1',
 author='Murtaza',
 author_email='murtazaraza336@gmail.com',
 packages=find_packages(),
-install_requires=['pandas', 'numpy']
-
+install_requires=get_requirements('requirements.txt')
 )
