b5ad6634ad3c0807fc02429d45b3ba841064b3d2
Murtaza Raza <murtazaraza336@gmail.com>
2024-04-08 19:12:01 +0100

    extracting data from the table's columns, formatting the data

--- src/components/web_scraping_cricket_data.py
+++ src/components/web_scraping_cricket_data.py
@@ -20,6 +20,40 @@ if response.status_code == 200:
         
     rows = table.find_all("tr")
 
-    print(rows)
+    for row in rows[1:]:
+        columns = row.find_all("td")
+        player_name = columns[0].text.strip()
+        span = columns[1].text.strip()
+        matches = int(columns[2].text.strip())
+        innings = int(columns[3].text.strip())
+        not_outs = int(columns[4].text.strip())
+        runs = int(columns[5].text.strip())
+        highest_score = columns[6].text.strip()
+        average = float(columns[7].text.strip())
+        bf = columns[8].text.strip()
+        sr = columns[9].text.strip()
+        centuries = int(columns[10].text.strip())
+        fifties = int(columns[11].text.strip())
+        zeros = int(columns[12].text.strip())
+        fours = columns[13].text.strip()
+        sixes = columns[14].text.strip()
+        
+        data.append({
+            "Player": player_name,
+            "Span": span,
+            "Matches": matches,
+            "Innings": innings,
+            "Not Outs": not_outs,
+            "Runs": runs,
+            "Highest Score": highest_score,
+            "Average": average,
+            "Balls Faced": bf,
+            "Strike Rate": sr,
+            "100s": centuries,
+            "50s": fifties,
+            "0s": zeros,
+            "4s": fours,
+            "6s": sixes
+        })
 
-# print(response)
+    print(data)
