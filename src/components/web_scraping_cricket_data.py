6fc5f22313647bb8f1e55417d0cd03005a6f952a
Murtaza Raza <murtazaraza336@gmail.com>
2024-04-08 18:18:18 +0100

    starting web scraping circket test match from espn

--- src/components/web_scraping_cricket_data.py
+++ src/components/web_scraping_cricket_data.py
@@ -1,9 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Cricket Test Matches Records
 url = "https://www.espncricinfo.com/records/highest-career-batting-average-282910"
 
 # Send a GET request to the URL
 response = requests.get(url)
 
-print(response)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, "html.parser")
+
+table = soup.find("table", class_="engineTable")
+
+if table:
+    print("Table found!")
+else:
+    print("Table not found!")
+
+# print(response)
