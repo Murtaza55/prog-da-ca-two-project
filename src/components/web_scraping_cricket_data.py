b43b36f83b580653b1d7ff62c96a6796a5cbf9f8
Murtaza Raza <murtazaraza336@gmail.com>
2024-04-08 18:23:53 +0100

    could not access the table, so changed the class after inspecting the element

--- src/components/web_scraping_cricket_data.py
+++ src/components/web_scraping_cricket_data.py
@@ -10,11 +10,11 @@ response = requests.get(url)
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, "html.parser")
 
-table = soup.find("table", class_="engineTable")
+    table = soup.find("table", class_="ds-table")
 
-if table:
-    print("Table found!")
-else:
-    print("Table not found!")
+    if table:
+        print("Table found!")
+    else:
+        print("Table not found!")
 
 # print(response)
