2057513db9fbb5591066d62d2427391a256ffad6
Murtaza Raza <murtazaraza336@gmail.com>
2024-04-11 12:35:17 +0100

    finding duplicates rows in data

--- src/components/data_cleaning.py
+++ src/components/data_cleaning.py
@@ -19,4 +19,12 @@ df = df.rename(columns={'Mat': 'Matches', 'NO': 'Not_Outs', 'HS': 'Highest_Score
 df['Balls_Faced'] = df['Balls_Faced'].fillna(0)
 df['Strike_Rate'] = df['Strike_Rate'].fillna(0)
 
-print(df[df['Player'] == 'CL Walcott (WI)'])
+# print(df[df['Player'] == 'CL Walcott (WI)'])
+
+#Now finding duplicates
+
+#returns true for some rows
+print(df.duplicated())
+
+#finding which rows are duplicated, returns 6 rows
+print(df[df['Player'].duplicated() == 1])
