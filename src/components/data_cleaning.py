25b73f04d2fcd9dae46db04f87104789c610ee5e
Murtaza Raza <murtazaraza336@gmail.com>
2024-04-11 11:58:36 +0100

    renaming column names from abreviations to full names

--- src/components/data_cleaning.py
+++ src/components/data_cleaning.py
@@ -3,3 +3,7 @@ import pandas as pd
 df = pd.read_csv('cricket_test_matches_records_data.csv')
 
 print(df)
+
+df = df.rename(columns={'Mat': 'Matches', 'NO': 'Not_Outs', 'HS': 'Highest_Score', 'Ave': 'Batting_Average', 'BF': 'Balls_Faced', 'SR': 'Strike Rate', '100': '100s', '50': '50s', '0': '0s', '4s': '4s'})
+
+print(df.head())
