1107800df249353436652806ef5afd218756fb2c
Murtaza Raza <murtazaraza336@gmail.com>
2024-04-11 11:59:53 +0100

    finding null data

--- src/components/data_cleaning.py
+++ src/components/data_cleaning.py
@@ -4,6 +4,10 @@ df = pd.read_csv('cricket_test_matches_records_data.csv')
 
 print(df)
 
+#renaming column names from abreviations to full names
 df = df.rename(columns={'Mat': 'Matches', 'NO': 'Not_Outs', 'HS': 'Highest_Score', 'Ave': 'Batting_Average', 'BF': 'Balls_Faced', 'SR': 'Strike Rate', '100': '100s', '50': '50s', '0': '0s', '4s': '4s'})
 
-print(df.head())
+# print(df.head())
+
+#finding null data if any
+print(df.isnull().any())
