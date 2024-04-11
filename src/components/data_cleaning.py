5dd17ef705d1dd4d40eab96ea9934fedba8fba42
Murtaza Raza <murtazaraza336@gmail.com>
2024-04-11 15:56:39 +0100

    cleaning rows, changing datatypes to appropriate ones
    - Removed * & + from column rows and also cleaning rows from other garbage.
    
    - Changed datatypes of each columns to appropriate ones.

--- src/components/data_cleaning.py
+++ src/components/data_cleaning.py
@@ -2,14 +2,15 @@ import pandas as pd
 
 df = pd.read_csv('cricket_test_matches_records_data.csv')
 
-print(df)
+# print(df)
 
-#renaming column names from abreviations to full names
+#=====renaming column names from abreviations to full names=========
 df = df.rename(columns={'Mat': 'Matches', 'NO': 'Not_Outs', 'HS': 'Highest_Score', 'Ave': 'Batting_Average', 'BF': 'Balls_Faced', 'SR': 'Strike_Rate', '100': '100s', '50': '50s', '0': '0s', '4s': '4s'})
 
 # print(df.head())
 
-#finding null data if any
+#====finding null data if any========
+
 # print(df.isnull().any())
 
 #Getting true for Balls_Faced and Strike rate, checking rows that have null values in those columns
@@ -21,7 +22,7 @@ df['Strike_Rate'] = df['Strike_Rate'].fillna(0)
 
 # print(df[df['Player'] == 'CL Walcott (WI)'])
 
-#Now finding duplicates
+#======Now finding duplicates=========
 
 #returns true for some rows
 # print(df.duplicated())
@@ -38,7 +39,7 @@ df = df.drop_duplicates()
 #now getting empty index, no more duplicates
 # print(df[df['Player'].duplicated() == 1])
 
-#splitting span column to Start Date and End Date
+#=====splitting span column to Start Date and End Date=====
 splitted_col = df['Span'].str.split(pat = '-')
 
 # print(splitted_col)
@@ -49,7 +50,8 @@ df['Final_Year'] = df['Span'].str.split(pat = '-').str[1]
 
 df = df.drop(['Span'], axis=1)
 
-#splitting the player name and country from player column
+#=======splitting the player name and country from player column=======
+
 # country_with_player = df['Player'].str.split(pat = ')').str[0]
 
 # country_without_player = df['Player'].str.split(pat = '(').str[1]
@@ -67,4 +69,39 @@ df['Player'] = df['Player'].str.split(pat = '(').str[0]
 
 # print(df['Player'])
 
-print(df.head())
+#====Working on correcting the data types
+
+# print(df.dtypes)
+
+#=====removing * and + from the Highest_Score and Balls Faced and all========
+
+# removing * from Highest_Score column rows
+df['Highest_Score'] = df['Highest_Score'].str.split(pat = '*').str[0]
+
+# removing * from Balls_Faced column rows
+df['Balls_Faced'] = df['Balls_Faced'].str.split(pat = '+').str[0]
+
+# removing * from Balls_Faced column rows
+df['4s'] = df['4s'].str.split(pat = '+').str[0]
+df['6s'] = df['6s'].str.split(pat = '+').str[0]
+
+#=====converting datatypes of columns from object to appropriate======
+
+df['Highest_Score'] = df['Highest_Score'].astype(int)
+
+#replacing more null data
+df['Balls_Faced'] = df['Balls_Faced'].fillna(0)
+# print(df.isnull().any())
+
+df['Balls_Faced'] = df['Balls_Faced'].astype(int)
+
+# print(df.dtypes)
+
+#changing multiple datatypes at once
+df = df.astype({'4s':'int', '6s':'int','Start_Year':'int', 'Final_Year':'int'})
+
+print(df.dtypes)
+
+# print(testing)
+
+# print(df.head())
